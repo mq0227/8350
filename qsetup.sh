ad1a3c25b305e6c77a2439693558d4abd923ec91
mq0227 <145292831+mq0227@users.noreply.github.com>
2025-07-08 21:49:30 +0800

    Create qsetup.sh

---
+++ qsetup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+BUILD_ROOT="$PWD"
+QSSI_ROOT="${BUILD_ROOT}/LA.QSSI.14.0"
+
+function sync_repo {
+    mkdir -p "$1" && cd "$1"
+    echo "[+] Changed directory to $1."
+
+    if repo init --depth=1 -q -u https://github.com/mq0227/8350.git -m "$2"; then
+        echo "[+] Repo initialized successfully."
+    else
+        echo "[-] Error: Failed to initialize repo."
+        exit 1
+    fi
+
+    echo "[+] Starting repo sync..."
+    if schedtool -B -e ionice -n 0 repo sync -q -c --force-sync --optimized-fetch --no-tags --retry-fetches=5 -j"$(nproc --all)"; then
+        echo "[+] Repo synced successfully."
+    else
+        echo "[-] Error: Failed to sync repo."
+        exit 1
+    fi
+}
+
+sync_repo "$QSSI_ROOT" "qssi.xml"
+
+cd "$BUILD_ROOT"
+echo "[+] Successfully returned to the build root."
